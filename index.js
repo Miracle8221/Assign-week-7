e5787a7b0b9f3c5889a03d9f7cffd53105bb6ff9
Miracle8221 <anagwumiracle098@gmail.com>
2023-09-06 13:41:11 +0100

    Finish creating the parent class

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ class Animal{
         this.species = species;
         this.sound = sound;
     }
+    // To make the animal sound
+    animalSound() {
+        console.log(this.sound)
+    }
 }
+// Create a subclass Dog to inherit from the Animal
+class Dog extends Animal {
+
+}
+
+const animal1 = new Animal('Dog', 'Woof-woof'); 
+console.log(animal1.animalSound())
