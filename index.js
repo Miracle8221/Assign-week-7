24e81acbfd996a97ddcbaed66377b60c28b46b3d
Miracle8221 <anagwumiracle098@gmail.com>
2023-09-06 13:52:48 +0100

    Add the animal sound

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ class Animal{
     }
     // To make the animal sound
     animalSound() {
-        console.log(this.sound)
+        console.log(this.sound);
     }
 }
-// Create a subclass Dog to inherit from the Animal
+// Create a subclass Dog to inherit from the Animal and a property color to the Dog
 class Dog extends Animal {
-
+    constructor(species, sound, color){
+        super(species, sound);
+        this.color = color;
+    }
+    animalSound(){
+        super.animalSound();
+        console.log(`Color: ${this.color}`)
+    }
 }
 
 const animal1 = new Animal('Dog', 'Woof-woof'); 
